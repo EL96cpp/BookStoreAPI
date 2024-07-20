ef16d62651fd25feab118a3672531bbddc5ee1b1
EL96cpp <el96cpp@gmail.com>
2024-07-20 13:04:46 +0300

    Added books applications, created and registered Book model, added books fixtures

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, null=False, verbose_name="Название")
+    author = models.CharField(max_length=255, null=False, verbose_name="Автор")
+    price = models.DecimalField(validators=[MinValueValidator(0)], max_digits=7, decimal_places=2, null=False, verbose_name="Цена")
+    discount = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(99)], default=0, null=True, verbose_name="Скидка")
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
