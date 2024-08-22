129c19a82565a3da3410441433d8373113f277c0
EL96cpp <el96cpp@gmail.com>
2024-08-22 16:26:59 +0300

    Added names for all of the urls, added tests for stores application

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,16 +16,16 @@ router = SimpleRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/book/<int:pk>', BookDetailView.as_view()),
-    path('api/v1/book-list', BookListView.as_view()),
-    path('api/v1/add-review/<int:book_id>', BookCreateReviewView.as_view()),
-    path('api/v1/book-reviews/<int:book_id>', BookReviewsListView.as_view()),
-    path('api/v1/profile/reviews', CustomerReviewsListView.as_view()),
-    path('api/v1/carts', CartView.as_view()),
-    path('api/v1/orders', OrderListView.as_view()),
-    path('api/v1/order/<int:order_id>', OrderRetrieveView.as_view()),
-    path('api/v1/order', OrderCreateView.as_view()),
-    path('api/v1/stores', StoreListView.as_view()),
+    path('api/v1/book/<int:pk>', BookDetailView.as_view(), name="book"),
+    path('api/v1/book-list', BookListView.as_view(), name="book-list"),
+    path('api/v1/add-review/<int:book_id>', BookCreateReviewView.as_view(), name="add-review"),
+    path('api/v1/book-reviews/<int:book_id>', BookReviewsListView.as_view(), name="book-review"),
+    path('api/v1/profile/reviews', CustomerReviewsListView.as_view(), name="reviews"),
+    path('api/v1/carts', CartView.as_view(), name="carts"),
+    path('api/v1/orders', OrderListView.as_view(), name="orders"),
+    path('api/v1/order/<int:order_id>', OrderRetrieveView.as_view(), name="order"),
+    path('api/v1/order', OrderCreateView.as_view(), name="order"),
+    path('api/v1/stores', StoreListView.as_view(), name="stores"),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
 ]
---
+++ stores/tests.py
@@ -0,0 +1,39 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import Store
+
+
+class StoresTests(APITestCase):
+
+    def setUp(self):
+        Store.objects.create(city="Moscow", address="Example st., 9")
+        Store.objects.create(city="Omsk", address="Example st., 15")
+
+
+    def test_stores_list(self):
+        response = self.client.get(reverse("stores"))
+        
+        expected_data = [
+            {'id': 1, 'city': 'Moscow', 'address': 'Example st., 9'}, 
+            {'id': 2, 'city': 'Omsk', 'address': 'Example st., 15'}
+        ]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_data)
+
+    
+    def test_city_filters(self):
+        
+        filter_moscow_response = self.client.get(reverse("stores"), {"city": "Moscow"})
+        expected_filter_moscow_data = [
+            {'id': 1, 'city': 'Moscow', 'address': 'Example st., 9'}, 
+        ]
+        self.assertEqual(filter_moscow_response.status_code, 200)
+        self.assertEqual(filter_moscow_response.data, expected_filter_moscow_data)
+
+        filter_omsk_response = self.client.get(reverse("stores"), {"city": "Omsk"})
+        expected_filter_omsk_data = [
+            {'id': 2, 'city': 'Omsk', 'address': 'Example st., 15'},
+        ]
+        self.assertEqual(filter_omsk_response.status_code, 200)
+        self.assertEqual(filter_omsk_response.data, expected_filter_omsk_data)
