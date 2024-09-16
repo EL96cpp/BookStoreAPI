2b059a33a7d5bb7d286f798ce8f2fbe1c693dc3f
EL96cpp <el96cpp@gmail.com>
2024-09-16 12:06:07 +0300

    Added redis cache to the BookListView and BookDetailView get methods

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets, generics
-from rest_framework.permissions import AllowAny
+from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .paginators import BookListPaginator
@@ -16,7 +17,15 @@ class BookListView(generics.ListAPIView):
     filterset_fields = ['price', 'discount']
     ordering_fields = ['title', 'price', 'discount']
 
+    @method_decorator(cache_page(30*60))
+    def get(self, *args, **kwargs):
+        return super().get(*args, **kwargs)
+
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    @method_decorator(cache_page(30*60))
+    def get(self, *args, **kwargs):
+        return super().get(*args, **kwargs)
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -210,5 +210,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'customers.Customer'
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        'OPTIONS': {
+            'db': '1',
+        }
+    }
+}
+
+
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
